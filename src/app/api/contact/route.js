e0b8b81b1c7ff5603d7f9fe03c13d210bbf942d2
Mehdi_QA <mtassi@medinplus.com>
2026-02-25 16:30:03 +0100

    add email contact form

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -28,7 +28,7 @@ export async function POST(req) {
 
     const { data, error } = await resend.emails.send({
         from: "onboarding@resend.dev",
-        to: "mehdi.tassi.pro@gmail.com",
+        to: "kaliops.consulting@gmail.com",
         subject: `📬 Nouveau message de ${name}`,
         replyTo: email,
         html: `
